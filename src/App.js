73ff2259c5c5b0c8ff604ccae7424f0f51cd36ff
Rajeevkavala <68290177+Rajeevkavala@users.noreply.github.com>
2024-08-15 12:09:00 +0530

    Completed
    Completed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CodemastersChatbot from './CodemastersChatbot';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <CodemastersChatbot />
+  </div>
+);
+
+export default App;
